bf7243efac9df6e93ffa53ee392fc9ab7fa0ddb5
Qui <nanoguy12@gmail.com>
2026-03-02 19:11:06 -0500

    added response interactions and general tweaks

--- scripts/interaction.gd
+++ scripts/interaction.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
-@export var timeline_name : String
+@export var interact_start : String
+@export var interact_bonus : String
+
+var has_been_interacted = false
 
 func interact():
-	print("INTERACT CALLED")
-	if timeline_name != "":
-		Dialogic.start(timeline_name)
+	if not has_been_interacted:
+		if interact_start != "":
+			Dialogic.start(interact_start)
+		has_been_interacted = true
+	else:
+		if interact_bonus != "":
+			Dialogic.start(interact_bonus)
