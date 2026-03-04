aa904ed1fa5c8dbe88fb207568ffbd6fe9b4047b
Qui <nanoguy12@gmail.com>
2026-03-04 04:32:30 -0500

    transition manager made

---
+++ scripts/TransitionManager.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+@onready var fade_rect = $ColorRect
+
+var is_transitioning = false
+
+func _ready():
+	fade_rect.anchor_left = 0
+	fade_rect.anchor_top = 0
+	fade_rect.anchor_right = 1
+	fade_rect.anchor_bottom = 1
+	fade_rect.color.a = 0
+
+func fade_to_scene(scene_path : String) -> void:
+	if is_transitioning:
+		return
+	is_transitioning = true
+	await _fade_out()
+	get_tree().change_scene_to_file(scene_path)
+	await _fade_in()
+	is_transitioning = false
+
+func _fade_out() -> void:
+	var steps = 4
+	var step_time = 0.09
+	for i in range(steps + 1):
+		fade_rect.color.a = i / float(steps)
+		await get_tree().create_timer(step_time).timeout
+	fade_rect.color.a = 1
+
+func _fade_in() -> void:
+	var steps = 4
+	var step_time = 0.09
+	for i in range(steps, -1, -1):
+		fade_rect.color.a = i / float(steps)
+		await get_tree().create_timer(step_time).timeout
+	fade_rect.color.a = 0
--- scripts/transition.gd
+++ scripts/transition.gd
@@ -1,1 +1,14 @@
 extends Area2D
+
+@export var next_scene : String
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_area_body_entered"))
+
+func _on_area_body_entered(body):
+	if body.is_in_group("Player"):
+		body.can_move = false
+		await _start_transition()
+
+func _start_transition() -> void:
+	await TransitionManager.fade_to_scene(next_scene)
