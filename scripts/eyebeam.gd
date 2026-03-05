3e5f4dd6818dc36b47ef41d113f06f82abeda7c7
Qui <nanoguy12@gmail.com>
2026-03-05 18:57:40 -0500

    made it so the if the player enters the eyebeam it resets the scene

---
+++ scripts/eyebeam.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var raycast_2D = $RayCast2D
+@onready var raycast_2D2 = $RayCast2D2
+@onready var raycast_2D3 = $RayCast2D3
+var fading := false
+
+func _physics_process(_delta):
+	if fading:
+		return
+
+	for raycast in [raycast_2D, raycast_2D2, raycast_2D3]:
+		if raycast.is_colliding():
+			var body = raycast.get_collider()
+			if body.is_in_group("Player"):
+				fading = true
+				var player = body
+				if body.get_parent().is_in_group("Player"):
+					player = body.get_parent()
+				player.set_physics_process(false)
+				await _fade_and_reload()
+				break
+
+func _fade_and_reload() -> void:
+	await TransitionManager._fade_out()
+	get_tree().reload_current_scene()
+	await TransitionManager._fade_in()
+	fading = false
--- scripts/path_follow_2d.gd
+++ scripts/path_follow_2d.gd
@@ -1,6 +1,6 @@
 extends PathFollow2D
 
-@export var speed: float = 0.09
+@export var speed: float = 0.11
 
 func _process(delta: float) -> void:
 	progress_ratio += speed * delta
